6d7f7d69fb0503396c60cc63ff08b0c7999f65da
Nick Inman <nick@theoatmeal.com>
2019-01-12 23:33:32 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,11 @@
-# Create your costume_stores migration here
+class CreateCostumeStores < ActiveRecord::Migration[5.1]
+  def change
+    create_table :costume_store do |t|
+      t.string :name
+      t.string :location
+      t.integer :costume_inventory
+      t.integer :size
+      t.timestamps
+    end
+  end
+end
