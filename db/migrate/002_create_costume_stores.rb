c4aa00fc46ce2cb3612c30a750614e1dfff0ce1f
Nick Inman <nick@theoatmeal.com>
2019-01-12 23:34:37 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -4,7 +4,11 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
       t.string :name
       t.string :location
       t.integer :costume_inventory
-      t.integer :size
+      t.integer :num_of_employees
+      t.boolean :still_in_business
+      
+
+
       t.timestamps
     end
   end
